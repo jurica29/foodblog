cceb7368fe21abf65373d2a78d1b63d097a3111c
jurica29 <jrcpuljiz@gmail.com>
2022-06-08 20:47:39 +0000

    Add recipe API keys and connect to index.html

--- blog/views.py
+++ blog/views.py
@@ -12,8 +12,9 @@ import requests
 def index(request):
     query = "cheese"
     response = requests.get("https://api.edamam.com/api/recipes/v2?type=public&q="+query+"&app_id=4f3bfbf8&app_key=3ae08ae7be7d3cec4d482eba3cc36dc8")
-    return render(request,"blog/index.html")
+    jsonResponse = response.json()
+    recipes = jsonResponse['hits']
+    return render(request,"blog/index.html", {'recipes': recipes})
 
 def specific(request):
     return HttpResponse("This is the specific url")
-
