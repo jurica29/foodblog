0ef66ee0d3bb0498a106b638ffc68e14befbf46b
jurica29 <jrcpuljiz@gmail.com>
2022-06-08 07:09:50 +0000

    Add API link to the views.py

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import requests
+
 # Create your views here.
+
+#4f3bfbf8
+#3ae08ae7be7d3cec4d482eba3cc36dc8
 
 
 def index(request):
+    query = "cheese"
+    response = requests.get("https://api.edamam.com/api/recipes/v2?type=public&q="+query+"&app_id=4f3bfbf8&app_key=3ae08ae7be7d3cec4d482eba3cc36dc8")
     return render(request,"blog/index.html")
 
 def specific(request):
