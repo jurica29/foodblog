61b0b3ea6b435ad779e9e5fad81f1f71b2e3184c
jurica29 <jrcpuljiz@gmail.com>
2022-06-09 21:23:04 +0000

    Create search function within urls and views.py

--- blog/views.py
+++ blog/views.py
@@ -18,3 +18,13 @@ def index(request):
 
 def specific(request):
     return HttpResponse("This is the specific url")
+
+def search(request):
+    if request.method == "POST":
+        userText = request.POST.get('userText')
+        response = requests.get("https://api.edamam.com/api/recipes/v2?type=public&q="+userText+"&app_id=4f3bfbf8&app_key=3ae08ae7be7d3cec4d482eba3cc36dc8")
+        jsonResponse = response.json()
+        recipes = jsonResponse['hits']
+        return render(request,"blog/index.html", {'recipes': recipes})    
+    else: 
+        return render(request, "blog/index.html")
