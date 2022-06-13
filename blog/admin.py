0dad66ea5937b0dd282f26c5129fc1105017b237
jurica29 <jrcpuljiz@gmail.com>
2022-06-13 18:35:00 +0000

    Customize data display in admin panel in admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from .models import Post
 
 # Registering models here
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status','created_on')
+    list_filter = ("status",)
+
+admin.site.register(Post, PostAdmin)
