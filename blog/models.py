729173dc98f92c1dbe810625674f69cd82ef2af3
jurica29 <jrcpuljiz@gmail.com>
2022-06-13 17:40:27 +0000

    Add blog post model to models.py

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
 
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    updated_on = models.DateTimeField(auto_now= True)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    status = models.IntegerField(choices=STATUS, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+class Meta:
+    ordering = ['-created_on']
+
+def __str__(self):
+    return self.title
