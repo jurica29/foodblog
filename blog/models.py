c0d0f1a82c0d2130bd4dda80f76fecb564b0c478
jurica29 <jrcpuljiz@gmail.com>
2022-06-13 18:19:15 +0000

    Fix issue with post name within django admin page

--- blog/models.py
+++ blog/models.py
@@ -17,8 +17,8 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     created_on = models.DateTimeField(auto_now_add=True)
 
-class Meta:
-    ordering = ['-created_on']
+    class Meta:
+        ordering = ['-created_on']
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
